f22c7e910825512cda13d77695646be663628c4b
Keith Moss <keithamoss@gmail.com>
2018-04-28 17:15:44 +0800

    2016 Spreadsheet to JSON metadata code update

--- census2016/generate-metadata-mapping.py
+++ census2016/generate-metadata-mapping.py
@@ -37,8 +37,18 @@ def getMetadataURLs(sheet):
     metadataUrls = []
     for row in sheet.iter_rows(row_offset=findOutMoreCell.row):
         cell = row[findOutMoreCell.col_idx - 1]
+        # print("cell.value", cell.value, cell)
         if cell.hyperlink is not None and cell.hyperlink.target is not None:
+            # print("Add", cell.value)
             metadataUrls.append({"name": cell.value, "url": cell.hyperlink.target})
+        else:
+            # Links with long names sometimes span two cells -
+            # so they're actually in the column to the right of this one.
+            cell = row[findOutMoreCell.col_idx - 2]
+            # print("cell.value", cell.value)
+            if cell.hyperlink is not None and cell.hyperlink.target is not None:
+                # print("Add", cell.value)
+                metadataUrls.append({"name": cell.value, "url": cell.hyperlink.target})
     return metadataUrls
 
 
@@ -65,24 +75,28 @@ def getMetadataURLs(sheet):
 
 def getNotes(sheet):
     def getFirstCellFromRow(row):
-        if sheetName == "T 32d" or sheetName == "T 33" or\
-                sheetName == "P 38b":
-            return row[1]  # Column A in these tables is hidden...Because Reasons
+        # if sheetName == "T 32d" or sheetName == "T 33" or\
+        #         sheetName == "P 38b":
+        #     return row[1]  # Column A in these tables is hidden...Because Reasons
         return row[0]
 
     def findNotesStartRow(sheet):
         firstRowWithAValue = None
         for row in reversed(list(sheet.iter_rows())):
             firstCell = getFirstCellFromRow(row)
+            # print("firstCell", firstCell, firstCell.value)
             if firstRowWithAValue is None and firstCell.value is not None:
                 # First row is also the only row with notes
+                # print("firstRowWithAValue")
                 if str(firstCell.value).startswith("This table is based"):
+                    # print("foo 1")
                     return firstCell.row - 1
-                firstRowWithAValue = firstCell.value
-            elif firstRowWithAValue is not None:
-                # The first row with no value signals the end of the notes section
-                if firstCell.value is None or str(firstCell.value).strip() == "":
-                    return firstCell.row
+            #     firstRowWithAValue = firstCell.value
+            # elif firstRowWithAValue is not None:
+            #     # The first row with no value signals the end of the notes section
+            #     if firstCell.value is None or str(firstCell.value).strip() == "":
+            #         # print("foo 2")
+            #         return firstCell.row
         raise Exception("Failed to find notes start row.")
 
     def findRowLabelsForNoteIdentifier(sheet, noteId):
@@ -94,8 +108,10 @@ def getNotes(sheet):
             if firstCell.value is None:
                 continue
 
-            # print("Checking {noteId} in '{rowVal}'".format(noteId=noteId, rowVal=firstCell.value))
-            match = re.search(r"^(?P<rowLabel>.+?)(?P<noteIdentifier>\({noteId}\))+[:]?".format(noteId=noteId), firstCell.value)
+            # print("Checking {noteId} in '{rowVal}' (cell {cell})".format(noteId=noteId, rowVal=firstCell.value, cell=firstCell))
+            # Coerce firstCell's value to string to deal with row labels that
+            # are purely numbers (e.g. ages)
+            match = re.search(r"^(?P<rowLabel>.+?)(?P<noteIdentifier>\({noteId}\))+[:]?".format(noteId=noteId), str(firstCell.value))
             if match is not None:
                 # print("Found!")
                 # print(match.groups(), "//", match.group("rowLabel").strip(), "//", match.group("noteIdentifier"))
@@ -129,19 +145,26 @@ def getNotes(sheet):
             # print
         return formattedNotes
 
-    # The whole ERP series has two tables and no notes in either
-    if sheetName == "E01" or sheetName == "E02":
-        return []
-
-    notesStartRow = findNotesStartRow(sheet)
+    # The Selected Medians and Averages tables have no "This table is based" line that
+    # we can use to identify where the notes start - so we hard code it.
+    if sheetName == "G 02":  # GCP
+        notesStartRow = 23
+    elif sheetName == "I 04":  # ATSIP
+        notesStartRow = 28
+    elif sheetName == "T 02":  # TSP
+        notesStartRow = 21
+    else:
+        notesStartRow = findNotesStartRow(sheet)
     # print("notesStartRow", notesStartRow)
 
     notes = []
     for row in sheet.iter_rows(row_offset=notesStartRow):
         firstCell = getFirstCellFromRow(row)
-        if firstCell.value is None:
-            break
-        notes.append(firstCell.value.strip())
+        if firstCell.value is not None:
+            notes.append(firstCell.value.strip())
+
+            if firstCell.value.startswith("Please note that there are small random adjustments"):
+                break
 
     notes = linkNotesAndColumns(sheet, notes)
     return notes
@@ -221,52 +244,64 @@ def mergeNotesFromMultipleProfileTables(notes, newNotes):
 
 #         print
 
+base_dir = "/app/data/2016 Datapacks"
 
-base_dir = "/Users/keithmoss/Documents/Work/GitHub/ealgis/data-loader/data/ealgis-aus-census-2011-master/2011 Datapacks BCP_IP_TSP_PEP_ECP_WPP_ERP_Release 3_Partial/Metadata/"
-# seriesName = "MALES"
+TABLES = [
+    ["GCP", "2016 General Community Profile", "2016_GCP_Sequential_Template.xlsx"],
+    ["PEP", "2016 Place of Enumeration Profile", "2016_PEP_Sequential_Template.xlsx"],
+    ["ATSIP", "2016 Aboriginal and Torres Strait Islander Peoples Profile", "2016_ATSIP_Sequential_Template.xlsx"],
+    ["TSP", "2016 Time Series Profile", "2016_TSP_Sequential_Template.xlsx"],
+    ["WPP", "2016 Working Population Profile", "2016_WPP_Sequential_Template.xlsx"],
+]
 
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with sequential_numbers_2011_BCP.xlsx"))
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with sequential_numbers_2011_IP.xlsx"))
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with sequential_numbers_2011_TSP.xlsx"))
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with_sequential_numbers_2011_ERP.xlsx"))
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with_sequential_numbers_2011_PEP.xlsx"))
-# xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with_sequential_numbers_2011_WPP.xlsx"))
-xl = openpyxl.load_workbook(os.path.join(base_dir, "Profile_template_with_sequential_numbers_2011_XCP.xlsx"))
-# print(xl.get_sheet_names())
+for table in TABLES:
+    abbreviation = table[0]
+    package_name = table[1]
+    xls_name = table[2]
 
+    xl = openpyxl.load_workbook(os.path.join(base_dir, package_name, "Metadata", xls_name))
 
-metadataMapping = {"tables": {}}
+    metadataMapping = {"tables": {}}
 
-for sheetName in xl.get_sheet_names()[1:]:
-    # if sheetName == "B 04" or sheetName == "B 02":
-    #     continue
-    # if not (sheetName == "I 01a" or sheetName == "I 01b"):
-    #     continue
-    # if sheetName != "T 32d":
-    #     continue
-    m = re.match('^([A-Za-z]+[0-9]+)([a-z]+)?$', sheetName.replace(" ", ""))
-    table_number = m.groups()[0]  # b46a -> b46
-    print("{} ({})".format(sheetName, table_number))
+    for sheetName in xl.get_sheet_names():
+        # if sheetName == "B 04" or sheetName == "B 02":
+        #     continue
+        # if not (sheetName == "I 01a" or sheetName == "I 01b"):
+        #     continue
+        # if sheetName == "G 02":
+        #     continue
+        # if sheetName != "G 03":
+        #     continue
+        # print("sheetName", sheetName)
 
-    sheet = xl[sheetName]
+        m = re.match('^([A-Za-z]+[0-9]+)([a-z]+)?$', sheetName.replace(" ", ""))
+        if m is None:
+            print("Skipping worksheet", sheetName)
+            continue
 
-    # serises = findSerises(sheet)
-    metadataUrls = getMetadataURLs(sheet)
-    notes = getNotes(sheet)
+        table_number = m.groups()[0]  # b46a -> b46
+        print("{} ({})".format(sheetName, table_number))
 
-    if table_number not in metadataMapping["tables"]:
-        metadataMapping["tables"][table_number] = {
-            "metadataUrls": metadataUrls,
-            "notes": notes,
-        }
-    else:
-        metadataMapping["tables"][table_number]["notes"] = mergeNotesFromMultipleProfileTables(metadataMapping["tables"][table_number]["notes"], notes)
+        sheet = xl[sheetName]
 
-for table_name in metadataMapping["tables"]:
-    metadataMapping["tables"][table_name]["notes"] = "<br />".join(metadataMapping["tables"][table_name]["notes"])
+        # serises = findSerises(sheet)
+        metadataUrls = getMetadataURLs(sheet)
+        notes = getNotes(sheet)
 
-with open("xcp_metadata_mapping.json", "w") as f:
-    f.write(json.dumps(metadataMapping, indent=2, sort_keys=True))
+        if table_number not in metadataMapping["tables"]:
+            metadataMapping["tables"][table_number] = {
+                "metadataUrls": metadataUrls,
+                "notes": notes,
+            }
+        else:
+            metadataMapping["tables"][table_number]["notes"] = mergeNotesFromMultipleProfileTables(metadataMapping["tables"][table_number]["notes"], notes)
+
+    for table_name in metadataMapping["tables"]:
+        metadataMapping["tables"][table_name]["notes"] = "<br />".join(metadataMapping["tables"][table_name]["notes"])
+
+    json_path = "metadata_mappings/{}_metadata_mapping.json".format(abbreviation.lower())
+    with open(json_path, "w") as f:
+        f.write(json.dumps(metadataMapping, indent=2, sort_keys=True))
 
 
 # Validate Metadata Mapping
@@ -286,4 +321,6 @@ with open("xcp_metadata_mapping.json", "w") as f:
 # B10a-b. Merged profileTableNames. Took notes from B10a.
 # B13a-b. Merged profileTableNames and notes by-hand. Manually set profileTableName to "B13" to match what's in the metadata.
 
-# Indigenous Profile
+
+# General Community Profile
+# Merged G02 (Selected Medians and Averages) by-hand
